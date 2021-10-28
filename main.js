69498c3385dc2f2a57135614e7a4575fc38213f9
GuysItsMeZach <61918546+GuysItsMeZach@users.noreply.github.com>
2021-10-27 23:51:40 -0500

    idk

--- main.js
+++ main.js
@@ -5,6 +5,15 @@
 // Output
 // NameCompatibility (from API) and ClubCompatibility (from matrix)
 // witty description 
+var person1name = getElementById(floatingInputGrid1)
+var person2name = getElementById(floatingInputGrid2)
+
+
+
+let count = 1;
+ 
+const value = document.querySelect("#cvalue")
+
 
 
 const descriptionDictionary = [
@@ -15,6 +24,9 @@ const descriptionDictionary = [
 ];
 
 function getDescription( score) {
+    if( score >= 10){
+        description = "not a chance, fool."
+    }
 
 }
 
@@ -41,4 +53,142 @@ const clubSelect = document.getElementById("clubSelect");
 clubSelect.addEventListener( "change" , () => {
     console.log( clubSelect.value );
 });
+
+function updateResults(c1, c2){
+    let results = cvalue;
+    //same club
+    if (c1 == ep && c2 == ep){
+        cvalue = 89
+    }
+    else if (c1 == xbx && c2 == xbx){
+        cvalue = 94
+    }
+    else if (c1 == on && c2 == on){
+        cvalue = 99
+    }
+    else if (c1 == pka && c2 == pka){
+        cvalue = 98
+    }
+    else if (c1 == xcd && c2 == xcd){
+        cvalue = 98
+    }
+    else if (c1 == athlete && c2 == athlete){
+        cvalue = 89
+    }
+    else if (c1 == chorale && c2 == chorale){
+        cvalue = 99
+    }
+    else if (c1 == none && c2 == none){
+        cvalue = 89
+    }
+    //same club
+    //ep
+    else if ((c1 == ep && c2 == xbx) || (c1 == xbx && c2 == ep)){
+        cvalue = 7
+    }
+    else if ((c1 == ep && c2 == on) || (c1 == on && c2 == ep)){
+        cvalue = 0
+    }
+    else if ((c1 == ep && c2 == pka) || (c1 == pka && c2 == ep)){
+        cvalue = 35
+    }
+    else if ((c1 == ep && c2 == xcd) || (c1 == xcd && c2 == ep)){
+        cvalue = 46
+    }
+    else if ((c1 == ep && c2 == athlete) || (c1 == athlete && c2 == ep)){
+        cvalue = 80
+    }
+    else if ((c1 == ep && c2 == chorale) || (c1 == chorale && c2 == ep)){
+        cvalue = 21
+    }
+    else if ((c1 == ep && c2 == none) || (c1 == none && c2 == ep)){
+        cvalue = 17
+    }
+    //ep
+    //xbx
+    else if ((c1 == xbx && c2 == on) || (c1 == on && c2 == xbx)){
+        cvalue = 68
+    }
+    else if ((c1 == xbx && c2 == pka) || (c1 == pka && c2 == xbx)){
+        cvalue = 57
+    }
+    else if ((c1 == xbx && c2 == xcd) || (c1 == xcd && c2 == xbx)){
+        cvalue = 68
+    }
+    else if ((c1 == xbx && c2 == athlete) || (c1 == on && c2 == athlete)){
+        cvalue = 9
+    }
+    else if ((c1 == xbx && c2 == chorale) || (c1 == chorale && c2 == xbx)){
+        cvalue = 92
+    }
+    else if ((c1 == xbx && c2 == none) || (c1 == none && c2 == xbx)){
+        cvalue = 68
+    }
+    //xbx
+    //on
+    else if ((c1 == on && c2 == pka) || (c1 == pka && c2 == on)){
+        cvalue = 10
+    }
+    else if ((c1 == on && c2 == xcd) || (c1 == xcd && c2 == on)){
+        cvalue = 3
+    }
+    else if ((c1 == on && c2 == athlete) || (c1 == athlete && c2 == on)){
+        cvalue = 2
+    }
+    else if ((c1 == on && c2 == chorale) || (c1 == chorale && c2 == on)){
+        cvalue = 98
+    }
+    else if ((c1 == on && c2 == none) || (c1 == none && c2 == on)){
+        cvalue = 93
+    }
+    //on
+    //pka
+    else if ((c1 == pka && c2 == xcd) || (c1 == xcd && c2 == pka)){
+        cvalue = 89
+    }
+    else if ((c1 == pka && c2 == athlete) || (c1 == athlete && c2 == pka)){
+        cvalue = 99
+    }
+    else if ((c1 == pka && c2 == athlete) || (c1 == athlete && c2 == pka)){
+        cvalue = 52
+    }
+    else if ((c1 == pka && c2 == none) || (c1 == none && c2 == pka)){
+        cvalue = 89
+    }
+    //pka
+    //xcd
+    else if ((c1 == xcd && c2 == athlete) || (c1 == athlete && c2 == xcd)){
+        cvalue = 67
+    }
+    else if ((c1 == xcd && c2 == chorale) || (c1 == chorale && c2 == xcd)){
+        cvalue = 52
+    }
+    else if ((c1 == xcd && c2 == none) || (c1 == none && c2 == xcd)){
+        cvalue = 7
+    }
+    //xcd
+    //athlete
+    else if ((c1 == athlete && c2 == chorale) || (c1 == chorale && c2 == athlete)){
+        cvalue = 2
+    }
+    else if ((c1 == athlete && c2 == none) || (c1 == none && c2 == athlete)){
+        cvalue = 9
+    }
+    //athlete
+    //chorale
+    else if ((c1 == chorale && c2 == none) || (c1 == none && c2 == chorale)){
+        cvalue = 28
+    }
+    //chorale
+    value.textContent = count
+}
+
+calcButton.addEventListener('click', function(e) {
+    count++
+    value.textContent = count;
+});
+
+
+
+
 
